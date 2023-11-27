b7c8df83fc1c235a04265f12bfeac4403f0eb44c
Lucas Eiki Gushiken <lushiken@gmail.com>
2023-11-27 18:01:12 -0300

    adicionado o arquivo variables.js e movendo algumas variáveis do index.js para o variables.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import { baseUrl, repositoriesQuantity } from "./variables.js"
+
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
     getUserProfile(userName)
@@ -13,12 +15,12 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
 })
 
 async function user(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
+    const response = await fetch(`${baseUrl}/${userName}`)
     return await response.json()
 }
 
 async function repos(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
+    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
     return await response.json()
 }
 
